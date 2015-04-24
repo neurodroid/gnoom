3d62ee1e6a875f33ab008c715a7b4ad6b9d11636
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2015-04-24 20:20:22 +0100

    Wait for correct pump response

--- py/pump.py
+++ py/pump.py
@@ -17,72 +17,75 @@
 
 import serial
 import sys
+import time
 
 sys.stdout.write("Opening serial port...")
 sys.stdout.flush()
-ser = serial.Serial('/dev/ttyS0', 19200, bytesize=8, parity='N', stopbits=1, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 19200, bytesize=8, parity='N', stopbits=1, timeout=None)
+time.sleep(2.0)
 # ser = serial.Serial('/dev/ttyUSB2', 19200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
 sys.stdout.write(" done\n")
 
-def write_bytes(string):
-    ser.write(bytearray(string, 'ascii'))
+def write_bytes(string, block=True):
+    resp = '1'
+    while resp != '0':
+        ser.write(bytearray(string, 'ascii'))
+        resp = ser.read().decode('utf-8')
+        if not block:
+            break
+        time.sleep(0.5)
+    print(resp)
+
+def read_bytes():
+    resp = ser.read()
+    return " Response: " + str(resp) + " " + resp.decode('utf-8') + "\n"
 
 def init():
     sys.stdout.write( "Resetting pump..." )
     sys.stdout.flush()
     write_bytes("*RESET\r\n")
-    ser.read()
-    sys.stdout.write( " done\n" )
+
     sys.stdout.write( "Setting syringe diameter..." )
     sys.stdout.flush()
     write_bytes("DIA 14.43\r\n") # B-D 10 ml
-    ser.read()
-    sys.stdout.write( " done\n" )
+
     sys.stdout.write( "Setting direction..." )
     sys.stdout.flush()
     write_bytes("DIR INF\r\n")
-    ser.read()
-    sys.stdout.write( " done\n" )
+
     sys.stdout.write( "Setting alarm..." )
     sys.stdout.flush()
     write_bytes("AL 0\r\n")
-    ser.read()
-    sys.stdout.write( " done\n" )
-    set_rate(05.00)
-    ser.read()
+
+    sys.stdout.write( "Setting rate..." )
+    sys.stdout.flush()
+    set_rate(8.3)
+
     # set_volume(10) # changed to 20 on 2012-01-17, CSH
-    set_volume(20.0) 
-    ser.read()
+    sys.stdout.write( "Setting volume..." )
+    sys.stdout.flush()
+    set_volume(20.0)
 
 def set_volume(vol):
     svol = "%05.2f" % vol
     sys.stdout.write( "Setting volume to %s ul..." % svol )
     sys.stdout.flush()
     write_bytes("VOL UL\r\n")
-    ser.read()
     write_bytes("VOL %s\r\n" % svol)
-    ser.read()
-    sys.stdout.write( " done\n" )
 
 def set_rate(rate):
     srate = "%05.2f" % rate
     sys.stdout.write( "Setting rate to %s..." % srate ) 
     sys.stdout.flush()
     write_bytes("RAT %s MM\r\n" % srate)
-    ser.read()
-    sys.stdout.write( " done\n" )
 
 def start_pump():
-    write_bytes("RUN\r\n")
-    ser.read()
-    write_bytes("BUZ 1 2\r\n")
-    ser.read()
+    write_bytes("RUN\r\n", False)
+    write_bytes("BUZ 1 2\r\n", False)
 
 def stop_pump():
-    write_bytes("STP\r\n")
-    ser.read()
-    write_bytes("STP\r\n")
-    ser.read()
+    write_bytes("STP\r\n", False)
+    write_bytes("STP\r\n", False)
 
 if __name__=="__main__":
     init()
