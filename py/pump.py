a08bc974542458bad60b635eea89752bfc6d03d6
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2015-04-24 10:11:14 +0100

    Fix serial communication with pump

--- py/pump.py
+++ py/pump.py
@@ -20,7 +20,7 @@ import sys
 
 sys.stdout.write("Opening serial port...")
 sys.stdout.flush()
-ser = serial.Serial('/dev/ttyS0', 19200, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 19200, bytesize=8, parity='N', stopbits=1, timeout=1)
 # ser = serial.Serial('/dev/ttyUSB2', 19200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
 sys.stdout.write(" done\n")
 
@@ -30,34 +30,37 @@ def write_bytes(string):
 def init():
     sys.stdout.write( "Resetting pump..." )
     sys.stdout.flush()
-    write_bytes("*RESET\r")
+    write_bytes("*RESET\r\n")
+    ser.read()
     sys.stdout.write( " done\n" )
     sys.stdout.write( "Setting syringe diameter..." )
     sys.stdout.flush()
-    write_bytes("DIA 14.43\r") # B-D 10 ml
+    write_bytes("DIA 14.43\r\n") # B-D 10 ml
     ser.read()
     sys.stdout.write( " done\n" )
     sys.stdout.write( "Setting direction..." )
     sys.stdout.flush()
-    write_bytes("DIR INF\r")
+    write_bytes("DIR INF\r\n")
     ser.read()
     sys.stdout.write( " done\n" )
     sys.stdout.write( "Setting alarm..." )
     sys.stdout.flush()
-    write_bytes("AL 0\r")
+    write_bytes("AL 0\r\n")
     ser.read()
     sys.stdout.write( " done\n" )
-    set_rate(09.00)
+    set_rate(05.00)
+    ser.read()
     # set_volume(10) # changed to 20 on 2012-01-17, CSH
     set_volume(20.0) 
+    ser.read()
 
 def set_volume(vol):
     svol = "%05.2f" % vol
     sys.stdout.write( "Setting volume to %s ul..." % svol )
     sys.stdout.flush()
-    write_bytes("VOL UL\r")
+    write_bytes("VOL UL\r\n")
     ser.read()
-    write_bytes("VOL %s\r" % svol)
+    write_bytes("VOL %s\r\n" % svol)
     ser.read()
     sys.stdout.write( " done\n" )
 
@@ -65,20 +68,20 @@ def set_rate(rate):
     srate = "%05.2f" % rate
     sys.stdout.write( "Setting rate to %s..." % srate ) 
     sys.stdout.flush()
-    write_bytes("RAT %s MM\r" % srate)
+    write_bytes("RAT %s MM\r\n" % srate)
     ser.read()
     sys.stdout.write( " done\n" )
 
 def start_pump():
-    write_bytes("RUN\r")
+    write_bytes("RUN\r\n")
     ser.read()
-    # write_bytes("BUZ 1 2\r")
-    # ser.read()
+    write_bytes("BUZ 1 2\r\n")
+    ser.read()
 
 def stop_pump():
-    write_bytes("STP\r")
+    write_bytes("STP\r\n")
     ser.read()
-    write_bytes("STP\r")
+    write_bytes("STP\r\n")
     ser.read()
 
 if __name__=="__main__":
